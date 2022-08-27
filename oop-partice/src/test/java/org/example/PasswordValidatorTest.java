7012a590581c6d74f73c82b255eacbbe7198f6e0
UMK <haga5032@naver.com>
2022-08-27 23:28:45 +0900

    객체지향 패러다임 코드 리뷰진행

--- oop-partice/src/test/java/org/example/PasswordValidatorTest.java
+++ oop-partice/src/test/java/org/example/PasswordValidatorTest.java
@@ -13,8 +13,18 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 * 비밀번호 8작 미만 또는 12자 초과인 경우 IllegolArgumentException 예외를 발생시킨다
 * 경계조건에 대해 테스트 코드를 작성해야 한다.
 * */
-public class PasswordValidatorTest {
 
+/*
+* 1. 문서화 역할
+* 2. 코드에 결함을 발견하는 역할
+* 3. 리팩토링 시 안정성 확보
+* 4. 테스 하기 쉬운 코드를 작성하다 보면 더 낮은 결합도를 가진 설계를 얻을 수 있음
+* */
+
+public class PasswordValidatorTest {
+        /*
+        1. 문서화 역할 -> 이러한 부분으로 어떤한 것을 테스트 하는지를 확인할 수 있습니다
+        */
         @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다")
         @Test
         void validatePasswordTest(){
